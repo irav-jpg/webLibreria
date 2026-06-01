43387d56fc778203faa5e574d1887e2db9da94f9
irav-jpg <junk61504@gmail.com>
2026-06-01 21:57:18 +0000

    inicio de sesión y dashboard 1

---
+++ src/viewModel/login.js
@@ -0,0 +1,41 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+
+    
+    const emailInput = document.getElementById('emailInput').value;
+    const passwordInput = document.getElementById('passwordInput').value;
+
+    
+    fetch('../data/users.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo cargar el archivo de usuarios.");
+            }
+            return response.json();
+        })
+        .then(data => {
+           
+            const usuarioEncontrado = data.usuarios.find(user => 
+                user.correo === emailInput && user.contrasena === passwordInput
+            );
+
+            if (usuarioEncontrado) {
+                
+                if (usuarioEncontrado.rol === 'admin') {
+                    alert('¡Bienvenido Administrador!');
+                    window.location.href = 'dashboard.html'; 
+                } else if (usuarioEncontrado.rol === 'cliente') {
+                    alert(`¡Hola de nuevo, ${usuarioEncontrado.nombre}!`);
+                    window.location.href = 'index.html'; 
+                }
+            } else {
+                
+                alert('Correo electrónico o contraseña incorrectos. Intenta de nuevo.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Hubo un problema al conectar con el sistema de usuarios.');
+        });
+});
